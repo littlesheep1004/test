fb6d1348ff0dbf456fe1f33060cbd91a05c0c400
littlesheep1004 <2879205362@qq.com>
2025-08-14 17:05:56 +0800

    Update test_demo.py

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,8 +1,15 @@
+import pytest
 import requests
 
-def test_baidu():
-    r = requests.get("https://www.baidu.com")
-    assert r.status_code == 200
+def add(a, b):
+    return a + b
 
-def test_math():
-    assert 1 + 1 == 2
+def test_add():
+    assert add(1, 2) == 3
+
+def test_add_negative():
+    assert add(-1, -2) == -3
+
+def test_httpbin_get():
+    response = requests.get("https://httpbin.org/get")
+    assert response.status_code == 200
